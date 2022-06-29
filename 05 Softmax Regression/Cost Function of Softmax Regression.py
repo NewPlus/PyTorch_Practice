4b8300fb4cf1c19b0ed7b8a02d919323a9100e35
NewPlus <lyhthy6@naver.com>
2022-06-29 22:30:15 +0900

    Softmax Regression (2022.06.29)

---
+++ 05 Softmax Regression/Cost Function of Softmax Regression.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn.functional as F
+torch.manual_seed(1)
+
+z = torch.FloatTensor([1, 2, 3])
+
+hypothesis = F.softmax(z, dim=0)
+print(hypothesis, hypothesis.sum())
+
+
+z = torch.rand(3, 5, requires_grad=True)
+
+hypothesis = F.softmax(z, dim=1)
+print(hypothesis)
+
+y = torch.randint(5, (3,)).long()
+print(y)
+
+y_one_hot = torch.zeros_like(hypothesis) 
+y_one_hot.scatter_(1, y.unsqueeze(1), 1)
+print(y_one_hot)
+
+cost = (y_one_hot * -torch.log(hypothesis)).sum(dim=1).mean()
+print(cost)
+
+######################## 위를 요약하는 교차 엔트로피 구현 #####################
+
+hypothesis = F.cross_entropy(z, y)
+print(hypothesis)
